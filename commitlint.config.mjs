f770914a8e62df7c22ff4c2afd545e779c47e217
Jordan Maulana <jordanmaulana26@gmail.com>
2024-11-05 12:22:01 +0700

    chore: add commitlint

---
+++ commitlint.config.mjs
@@ -0,0 +1,18 @@
+// export default { extends: ["@commitlint/config-conventional"] };
+
+const Configuration = {
+  /*
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "style", "refactor", "test", "chore"],
+    ],
+  },
+};
+
+export default Configuration;
