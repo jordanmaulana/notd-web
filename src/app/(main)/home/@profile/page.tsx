14a1b6ac167144102ee4671bb30006e3a5745755
Jordan Maulana <jordanmaulana26@gmail.com>
2024-11-09 16:42:56 +0700

    feat: logout

--- src/app/(main)/home/@profile/page.tsx
+++ src/app/(main)/home/@profile/page.tsx
@@ -3,13 +3,18 @@ import { getProfile } from "@/features/profile/repo/profile_repo";
 
 import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import React from "react";
+import { logout } from "./action";
 
 export default async function Page() {
   const { data } = await getProfile();
 
   return (
-    <div className="flex w-60 items-center justify-between gap-4 rounded-full px-4 py-3 font-bold hover:bg-slate-100/10">
+    <div
+      className="flex w-60 cursor-pointer items-center justify-between gap-4 rounded-full px-4 py-3 font-bold hover:bg-slate-100/10"
+      onClick={logout}
+    >
       <div className="flex gap-4">
         <VAvatar />
         <div>{data?.name}</div>
